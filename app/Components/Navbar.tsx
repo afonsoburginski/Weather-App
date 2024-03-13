8a85aff791a7815e11bb033efd646241f458c437
afonsoburginski <afonsoburginski@gmail.com>
2024-03-13 13:19:07 -0400

    Atualiza o link do botão "Source Code" para redirecionar para o repositório do GitHub

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -23,7 +23,7 @@ function Navbar() {
           <Button
             className="source-code-btn flex items-center gap-2"
             onClick={() => {
-              router.push("https//github.com");
+              router.push("https://github.com/afonsoburginski/Weather-App");
             }}
           >
             {github} Source Code
